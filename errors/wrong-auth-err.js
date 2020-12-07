4619c2eb38c0958a60ed44519e0d4d3434989d7f
Dmitriy Brovchenko <dbrov@mail.ru>
2020-12-07 22:36:16 +0300

    Final

--- errors/wrong-auth-err.js
+++ errors/wrong-auth-err.js
@@ -1,8 +1,8 @@
-class NotAuthorizedError extends Error {
+class WrongUserError extends Error {
   constructor(message) {
     super(message);
     this.statusCode = 401;
   }
 }
 
-module.exports = NotAuthorizedError;
+module.exports = WrongUserError;
--- schemas/tokenSchema.js
+++
@@ -1,9 +0,0 @@
-const { Joi, Segments } = require('celebrate');
-
-const tokenSchema = {
-  [Segments.HEADERS]: Joi.object({
-    authorization: Joi.string().required().regex(/^Bearer /),
-  }).unknown(true),
-};
-
-module.exports = tokenSchema;
